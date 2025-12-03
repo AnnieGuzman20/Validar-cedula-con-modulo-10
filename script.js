ba021094e337763934dbf2e63efb45f5acc5ec4c
AnnieGuzman20 <linnett4602@gmail.com>
2025-12-02 21:33:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+async function validar() {
+  const cedula = document.getElementById("cedula").value.trim();
+  const resultado = document.getElementById("resultado");
+
+  if (!/^\d{11}$/.test(cedula)) {
+    resultado.textContent = "❌ CÉDULA INCORRECTA (debe tener 11 dígitos)";
+    return;
+  }
+
+  try {
+
+    const url = `http://servicioweb-modulo10.infinityfree.me/validarCedula.php?cedula=${cedula}`;
+
+    const respuesta = await fetch(url);
+    const data = await respuesta.json();
+
+    resultado.textContent = data.mensaje;
+
+  } catch (error) {
+    resultado.textContent = "⚠️ Error al conectar con el servicio web";
+  }
+}
